c38dde61b5139959cd1664203a2c60aad8301c81
Mike Kazantsev <mk.fraggod@gmail.com>
2015-05-02 23:01:57 +0500

    Revert "Use yaml.safe_load" - SafeLoader is used already here
    This reverts commit 675cb141a49a31fd93f502b7e197cec340b0b68b.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'layered-yaml-attrdict-config',
-	version = '15.5.0',
+	version = '15.5.2',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',
