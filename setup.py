66ed1df3b96cd0e816afbf209d280d644dafeba8
Mike Kazantsev <mk.fraggod@gmail.com>
2015-09-08 22:16:14 +0500

    README: convert to rst

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'layered-yaml-attrdict-config',
-	version = '15.9.1',
+	version = '15.9.2',
 	author = 'Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'WTFPL',
